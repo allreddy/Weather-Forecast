77641e8d1916150196dd590ffaec1984f5ebdaa8
allreddy <nithin@enplug.com>
2018-11-23 20:17:25 -0800

    Build weather list container and install sparklines for display

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,12 @@
 const API_KEY = '53e00653cdf8c785d0ae3f956241111b';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-const FETCH_WEATHER = 'FETCH_WEATHER';
+export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 import axios from 'axios';
 
 export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
-
-    console.log('Request: ', request);
 
     return {
         'type': FETCH_WEATHER,
---
+++ src/containers/weather_list.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class WeatherList extends Component {
+    renderWeather(cityData) {
+        const name = cityData.city.name;
+        const temps = cityData.list.map(weather => weather.main.temp);
+        console.log(temps);
+        //const pressures = cityData.list.map(pressure => pressure.main.pressure);
+
+
+        return (
+            <tr>
+                <td key={name}>
+                    {name}
+                </td>
+            </tr>
+        );
+    }
+
+    render() {
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>City</th>
+                        <th>Temperature</th>
+                        <th>Humidity</th>
+                        <th>Pressure</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.props.weather.map(this.renderWeather)}
+                </tbody>
+            </table>
+        );
+    }
+}
+
+function mapStateToProps({weather}) {
+    return {weather};
+}
+
+export default connect(mapStateToProps)(WeatherList);
