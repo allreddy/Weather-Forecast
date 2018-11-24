955d0187aefb3c69233d2ec02a8204966d7eaac1
allreddy <nithin@enplug.com>
2018-11-23 20:37:49 -0800

    Working charts plotting temp, pressure and humidity

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,22 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const name = cityData.city.name;
         const temps = cityData.list.map(weather => weather.main.temp);
-        console.log(temps);
-        //const pressures = cityData.list.map(pressure => pressure.main.pressure);
-
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr>
                 <td key={name}>
                     {name}
                 </td>
+                <td><Chart data={temps} color="orange"/></td>
+                <td><Chart data={pressures} color="blue"/></td>
+                <td><Chart data={humidities} color="green"/></td>
             </tr>
         );
     }
